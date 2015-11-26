a87057774e66254ac527642cdcc804ae05164887
Sam Schmidt <samuel@dersam.net>
2015-11-26 15:08:40 -0500

    Module setup.

---
+++ src/app/code/local/Linus/Kickbox/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ *
+ * @author Sam Schmidt <samuel@dersam.net>
+ * @since 2015-11-26
+ * @company Linus Shops
+ */
+class Linus_Kickbox_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
