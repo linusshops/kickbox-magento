f057da1613524864e6138c23a3467b8afaf2f3c1
Sam Schmidt <samuel@dersam.net>
2015-11-27 16:30:47 -0500

    Whitespace.

--- src/app/code/local/Linus/Kickbox/Helper/Data.php
+++ src/app/code/local/Linus/Kickbox/Helper/Data.php
@@ -12,6 +12,7 @@ class Linus_Kickbox_Helper_Data extends Mage_Core_Helper_Abstract
     /**
      * Verify an email against Kickbox.io. Returns the response model. Use this
      * if you require access to the fields returned by Kickbox.
+     * 
      * @param string $email
      * @param array $options
      * @return Linus_Kickbox_Model_Email
@@ -29,6 +30,7 @@ class Linus_Kickbox_Helper_Data extends Mage_Core_Helper_Abstract
      * the email is valid.  Use this if you just want to know if the email
      * can be delivered.  'deliverable' and 'risky' will both be considered
      * valid and return true.
+     *
      * @param string $email
      * @param array $options
      * @return bool
